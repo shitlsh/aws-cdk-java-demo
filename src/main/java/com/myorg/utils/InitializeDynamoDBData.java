fd9d9004bc14dd07c556de2b43eecea134ae884b
Tailong <shitlsh@126.com>
2021-07-27 17:28:11 +0800

    daily commit

--- src/main/java/com/myorg/utils/InitializeDynamoDBData.java
+++ src/main/java/com/myorg/utils/InitializeDynamoDBData.java
@@ -3,18 +3,34 @@ package com.myorg.utils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class InitializeDynamoDBData {
+
+    private static final String[] salutationsMale = {"sir", "sweetheart", "darling", "swain", "suitor", "sire", "gentleman", "monsieur", "baby"};
+    private static final String[] salutationsFemale = {"dame", "sweetheart", "delicacy", "darling", "angel", "moppet", "missy", "lass", "baby"};
+
     public static JSONObject initializeSalutations(String tableName) {
         JSONArray putRequests = new JSONArray();
-        putRequests.put(new JSONObject().put("PutRequest", Map.ofEntries(
-                Map.entry("Item", Map.ofEntries(
-                        Map.entry("itemId", Map.of("S", "1")),
-                        Map.entry("itemType", Map.of("S", "salutation")),
-                        Map.entry("content", Map.of("S", "sweetheart"))
-                ))
-        )));
+        Arrays.stream(salutationsMale).forEach(salutation ->
+                putRequests.put(new JSONObject().put("PutRequest", Map.ofEntries(
+                        Map.entry("Item", Map.ofEntries(
+                                Map.entry("itemId", Map.of("S", "1")),
+                                Map.entry("itemType", Map.of("S", "salutation")),
+                                Map.entry("content", Map.of("S", salutation))
+                        ))
+                )))
+        );
+        Arrays.stream(salutationsFemale).forEach(salutation ->
+                putRequests.put(new JSONObject().put("PutRequest", Map.ofEntries(
+                        Map.entry("Item", Map.ofEntries(
+                                Map.entry("itemId", Map.of("S", "1")),
+                                Map.entry("itemType", Map.of("S", "salutation")),
+                                Map.entry("content", Map.of("S", salutation))
+                        ))
+                )))
+        );
         return new JSONObject().put(tableName, putRequests);
     }
 }
